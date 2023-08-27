784e7babeed50914d3190c1469f8f8dbab1db37a
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-08-26 19:06:53 -0700

    Created and updated server.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
 
-const blogpostRoutes = require('./blogpostRoutes');
+const blogRoutes = require('./blogRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/comment', commentRoutes);
 
-router.use('/blogposts', blogpostRoutes);
+router.use('/blogposts', blogRoutes);
 
 module.exports = router;
